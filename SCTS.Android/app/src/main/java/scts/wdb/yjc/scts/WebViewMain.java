6a31b9aef954bca4b8147f17a98217f7fd705d8b
hs961231 <hs961231@naver.com>
2017-05-10 22:52:44 +0900

    Revert "2017"
    This reverts commit f07b7b2532b3af8c07999151817511cb025aebad.

--- SCTS.Android/app/src/main/java/scts/wdb/yjc/scts/WebViewMain.java
+++ SCTS.Android/app/src/main/java/scts/wdb/yjc/scts/WebViewMain.java
@@ -3,10 +3,14 @@ package scts.wdb.yjc.scts;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.view.View;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -88,13 +92,9 @@ public class WebViewMain extends AppCompatActivity {
                     .setPositiveButton("예", new DialogInterface.OnClickListener(){
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-
+                            android.os.Process.killProcess(android.os.Process.myPid());
                             editor = sp.edit();
                             editor.remove("user_id");
-                            moveTaskToBack(true);
-                            android.os.Process.killProcess(android.os.Process.myPid());
-
-
 
                         }
                     })
