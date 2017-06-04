f821d0868a8dbf0f7f41cebe0c7127ad11681041
hs961231 <hs961231@naver.com>
2017-06-04 11:53:21 +0900

    2017_06_04_02 정혜수
    전에거 오류나서 고쳐서 다시 올림
    안드로이드 컨트롤러 거의 완성

--- SCTS.STS/SCTS/src/main/java/yjc/wdb/scts/service/BillService.java
+++ SCTS.STS/SCTS/src/main/java/yjc/wdb/scts/service/BillService.java
@@ -1,5 +1,15 @@
 package yjc.wdb.scts.service;
 
+import java.util.HashMap;
+import java.util.List;
+
+import yjc.wdb.scts.bean.BillVO;
+
 public interface BillService {
+	
+	public List<BillVO> billList(String user_id, int day) throws Exception;
+	public List<HashMap> billOne(int bill_code) throws Exception;
+	public List<HashMap> settleInfo(String user_id) throws Exception;
+	public List<HashMap> recommandProduct(String user_id) throws Exception;
 
 }
---
+++ SCTS.STS/SCTS/src/main/java/yjc/wdb/scts/service/impl/BillServiceImpl.java
@@ -0,0 +1,44 @@
+package yjc.wdb.scts.service.impl;
+
+import java.util.HashMap;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import yjc.wdb.scts.bean.BillVO;
+import yjc.wdb.scts.dao.BillDAO;
+import yjc.wdb.scts.service.BillService;
+
+@Service
+public class BillServiceImpl implements BillService {
+	
+	@Inject
+	private BillDAO dao;
+
+	@Override
+	public List<BillVO> billList(String user_id, int day) throws Exception {
+		
+		return dao.billList(user_id, day);
+	}
+
+	@Override
+	public List<HashMap> billOne(int bill_code) throws Exception {
+		
+		return dao.billOne(bill_code);
+	}
+
+	@Override
+	public List<HashMap> settleInfo(String user_id) throws Exception {
+		
+		return dao.settleInfo(user_id);
+	}
+
+	@Override
+	public List<HashMap> recommandProduct(String user_id) throws Exception {
+		
+		return dao.recommandProduct(user_id);
+	}
+
+}
