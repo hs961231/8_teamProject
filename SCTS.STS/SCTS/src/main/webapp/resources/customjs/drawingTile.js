20c1246293cd5b4d4e45508807b500411ef1aa2c
zseilx <zseil@naver.com>
2017-06-26 10:52:38 +0900

    2017_06_26 정영화

--- SCTS.STS/SCTS/src/main/webapp/resources/customjs/drawingTile.js
+++ SCTS.STS/SCTS/src/main/webapp/resources/customjs/drawingTile.js
@@ -28,7 +28,66 @@ var imgLoad = function(floor) {
 
 				$("#drw_code").val(data.drw_code);
 
+				var tileMap = $(".tileMap");
 
+				tileMap.empty();
+
+				// 해당 층의 설정된 타일 갯수까지 가져올 수 있어야 함.
+				for(var i=0; i<data.size_y; i++) {
+					var tileRow = $("<div></div>");
+
+					//tileRow.css("width", "100%");
+					//tileRow.css("height", heightSize + "%");
+
+					for(var j=0; j<data.size_x; j++) {
+						var tileItem = $("<div></div>").addClass("tile");
+						//tileItem.css("width", widthSize + "%");
+						//tileItem.css("height", "100%");
+						//tileItem.css("float", "left");
+
+						tileItem.appendTo(tileRow);
+					}
+					tileRow.appendTo(tileMap);
+				}
+				var heightSize = 100 / data.size_y;
+				var widthSize = 100 / data.size_x;
+				$(".tileMap > div").css("width", "100%").css("height", heightSize + "%");
+				$(".tile").css("width", widthSize + "%").css("height", "100%").css("float", "left");
+
+
+				/* 타일별로 색깔 저장 해주는 것 */
+				var tileInfoList = data.tileInfoList;
+				var grade = tileInfoList.length / 3;
+
+
+				for(var i=0; i<tileInfoList.length; i++) {
+					var info = tileInfoList[i];
+
+					var x = info.TILE_CRDNT_X;
+					var y = info.TILE_CRDNT_Y;
+
+					var row = $("div.tileMap > div").eq(x);
+					var col = row.find("div.tile").eq(y);
+
+					var alpha = 0.05;
+					/*if(info.probability > 0.4) {
+			alpha = 0.5;
+		}*/
+					if(i < grade) {
+						alpha = 0.8;
+					}
+					else if(i < grade*2) {
+						alpha = 0.5;
+					}
+					else {
+						alpha = 0.2
+					}
+
+					col.text( (info.probability*100) + "%" );
+					col.css("background-color", "#" + info.LCLASCTGRY_COLOR);
+					//col.css("opacity", 0.1 + (info.probability * 0.5));
+					col.css("opacity", alpha);
+				}
 
 
 			}
