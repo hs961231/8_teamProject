fa7bb9e1ae900335d4a9e3300cbe6ac9dfb3df8c
hs961231 <hs961231@naver.com>
2017-07-12 12:00:12 +0900

    2017_07_12_01 정혜수

--- SCTS.STS/SCTS/src/main/java/yjc/wdb/scts/dao/CourseDAO.java
+++ SCTS.STS/SCTS/src/main/java/yjc/wdb/scts/dao/CourseDAO.java
@@ -9,11 +9,11 @@ import yjc.wdb.scts.bean.TileVO;
 
 public interface CourseDAO {
 	public void insertCourse(HashMap<String, String> vo) throws Exception;
-	public int selectTodayVisitCnt() throws Exception;
+	public int selectTodayVisitCnt(int bhf_code) throws Exception;
 	public HashMap<String, String> tileProbability(TileVO vo) throws Exception;
 	public List<HashMap<String, String>> tileUserinfo(TileVO vo) throws Exception;
 	public void updateStayTime(HashMap<String, String> vo) throws Exception;
-	public int monthAvgVisitor() throws Exception;
+	public int monthAvgVisitor(int bhf_code) throws Exception;
 	public List<HashMap> tileGender(int day) throws Exception;
 	public List<HashMap> tileAge(int day) throws Exception;
 	public List<HashMap> oneTileAvgTime(int day, int drw_code, int tile_crdnt_x, int tile_crdnt_y) throws Exception;
